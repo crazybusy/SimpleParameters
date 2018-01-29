7ae5a68acd2ba2d50517d24e11c694d7dc0958f5
Saurabh <upscprep48@gmail.com>
2018-01-29 20:08:37 +0530

    Make ready for upload to pypi. Rename to lower_case instead of CamelCase, Create source folder for sources

---
+++ setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name = 'SimpleParameters',
+    packages = ['src'],
+    version = '1.1',
+    description = 'Simple parsing of parameters supplied on the command lin',
+    author='Saurabh Gupta',
+    author_email = 'upscprep48@gmail.com',
+    url = 'https://github.com/crazybusy/SimpleParameters',
+    long_description= 'Allows to create json file with details of the parameters available on the command line for any application. It then handles the user parsing of these parameters by setting the required flags which you can directly consume in your application',
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: Free for non-commercial use",
+        "Topic :: Software Development :: Libraries"
+        "Topic :: Utilities",
+        "Topic :: Text Processing"
+        ]
+)
