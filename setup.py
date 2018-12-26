1a8bda97daa4abeafc4715eb89e2d03c37b63f2e
Saurabh <upscprep48@gmail.com>
2018-12-26 19:47:48 +0530

    Fixed setup.py for the distributions

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from distutils.core import setup
-setup(
+import setuptools
+
+setuptools.setup(
     name = 'SimpleParameters',
-    packages = ['simple_parameters'],
-    version = '1.1',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    version = '1.2',
     description = 'Simple parsing of parameters supplied on the command line',
-    author='Saurabh Gupta',
+    author='crazybusy',
     author_email = 'upscprep48@gmail.com',
     url = 'https://github.com/crazybusy/SimpleParameters',
     long_description= 'Allows to create json file with details of the parameters available on the command line for any application. It then handles the user parsing of these parameters by setting the required flags which you can directly consume in your application',
@@ -12,9 +14,6 @@ setup(
         "Intended Audience :: Developers",
         "Intended Audience :: Information Technology",
         "Intended Audience :: System Administrators",
-        "License :: Free for non-commercial use",
-        "Topic :: Software Development :: Libraries"
-        "Topic :: Utilities",
-        "Topic :: Text Processing"
+        "License :: Free for non-commercial use"
         ]
 )
